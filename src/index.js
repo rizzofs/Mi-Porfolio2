d56275702eefbfa2e167cd5ca9b11d0f6d3584d6
rizzofs <rizzofs@gmail.com>
2025-07-14 22:55:24 -0300

    Feat: utimo cambio por hoy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Mi-Porfolio2">
       <App />
     </BrowserRouter>
   </React.StrictMode>
